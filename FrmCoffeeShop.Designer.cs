841ac9628ab8258182e18bfdfb3af6b179f20cd0
DangHongThy <1554050096thy@ou.edu.vn>
2019-03-05 09:53:01 +0700

    update

---
+++ FrmCoffeeShop.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace demo
+{
+    partial class FrmCoffeeShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHAO MUNG DEN VOI COFFEESHOP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FrmCoffeeShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 337);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCoffeeShop";
+            this.Text = "FrmCoffeeShop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
