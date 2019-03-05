f319f0cb3bc7a26144c6eaf628264d3928d58819
DangHongThy <1554050096thy@ou.edu.vn>
2019-03-05 10:33:43 +0700

    update5

--- FrmCoffeeShop.Designer.cs
+++ FrmCoffeeShop.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(216, 208);
+            this.button1.Location = new System.Drawing.Point(219, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 37);
             this.button1.TabIndex = 1;
